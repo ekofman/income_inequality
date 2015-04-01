ef2aac4fd1c499d57c5a1f73d54e0fdbd9fda41b
Eric Kofman <ericrkofman@gmail.com>
2015-03-31 23:34:25 -0700

    better json data structure

--- convert_to_json.py
+++ convert_to_json.py
@@ -18,7 +18,8 @@ with open('data.csv', 'rb') as csvfile:
   			average_top_1 = (row[22] if row[22]!='' else None)
   			average_top_01 = (row[26] if row[26]!='' else None)
   			if not country_dict[country]:
-  				data_dict = {'share_top_10': [],
+  				data_dict = {'name': country,
+                 'share_top_10': [],
   							 'share_top_1': [],
   				 			 'share_top_01': [],
   							 'average_top_10': [],
@@ -26,16 +27,16 @@ with open('data.csv', 'rb') as csvfile:
   							 'average_top_01': []}
   				country_dict[country] = data_dict
 			if share_top_10 is not None:
-				country_dict[country]['share_top_10'].append({'year':year, 'value': share_top_10})
+				country_dict[country]['share_top_10'][year] = share_top_10
 			if share_top_1 is not None:
-  				country_dict[country]['share_top_1'].append({'year':year, 'value': share_top_1})			
+  				country_dict[country]['share_top_1'][year] =share_top_1			
 			if share_top_01 is not None:	
-  				country_dict[country]['share_top_01'].append({'year': year, 'value': share_top_01})
+  				country_dict[country]['share_top_01'][year] = share_top_01
   			if average_top_10 is not None:
-				country_dict[country]['average_top_10'].append({'year': year, 'value': average_top_10})
+				country_dict[country]['average_top_10'][year] = average_top_10
 			if average_top_1 is not None:
-  				country_dict[country]['average_top_1'].append({'year': year, 'value': average_top_1})		
+  				country_dict[country]['average_top_1'][year] = average_top_1		
   			if average_top_01 is not None:	
-  				country_dict[country]['average_top_01'].append({'year' : year, 'value': average_top_01})
+  				country_dict[country]['average_top_01'][year] = average_top_01
 
 json.dump(country_dict, jsonfile)
