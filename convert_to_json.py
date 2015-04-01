3a5232e35358444252dc7a41da272bb6b67cd4de
Eric Kofman <ericrkofman@gmail.com>
2015-03-31 23:35:11 -0700

    use dicts instead of strings in python converter

--- convert_to_json.py
+++ convert_to_json.py
@@ -19,12 +19,12 @@ with open('data.csv', 'rb') as csvfile:
   			average_top_01 = (row[26] if row[26]!='' else None)
   			if not country_dict[country]:
   				data_dict = {'name': country,
-                 'share_top_10': [],
-  							 'share_top_1': [],
-  				 			 'share_top_01': [],
-  							 'average_top_10': [],
-  							 'average_top_1': [],
-  							 'average_top_01': []}
+                 'share_top_10': {},
+  							 'share_top_1': {},
+  				 			 'share_top_01': {},
+  							 'average_top_10': {},
+  							 'average_top_1': {},
+  							 'average_top_01': {}}
   				country_dict[country] = data_dict
 			if share_top_10 is not None:
 				country_dict[country]['share_top_10'][year] = share_top_10
